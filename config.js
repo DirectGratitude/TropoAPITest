d0f297116a81b747a6b40c40d59c33556db0f6cb
Tommy Bergeron <tomshreds@me.com>
2012-06-06 14:39:25 -0400

    comments

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ process.env['MONGODB_DATABASE'] = 'TropoAPITest';
 process.env["MONGODB_USER"] = 'testuser';
 process.env["MONGODB_PASSWORD"] = 'testpassword';
 
-
+// Tropo Tokens
 process.env["TROPO_CALL_TOKEN"] = '11bc7291593c6a408b6ca416ff478f269056f8bd7d4656633a9b289f0426d22bcf72ae2edfb3fb71dd673e10';
 process.env["TROPO_MESSAGE_TOKEN"] = '11bc2ebce55e1142a9198cfc82c25475b653b08315c3a907eb3df8bf795be9cf4a25d4312290c409000555de';
