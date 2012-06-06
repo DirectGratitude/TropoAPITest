583e8af3faab197c77dc05da4aa2d9c373b2466a
Tommy Bergeron <tomshreds@me.com>
2012-06-06 16:04:31 -0400

    ah yeah

--- app/controllers/message.js
+++ app/controllers/message.js
@@ -11,50 +11,50 @@ var MessageController = createController({
 
   // /messages/new
   new: function(req, res) {
-    var action = "call";
-    var message = "Do not miss your rendez-vous with Sabrina at 3pm. Love you. Pommy.";
-    var number = "18198018805";
-
-    // todo : reselect action when form has errors
-    // todo : fix sms (problems seems to be about outbound sms)
-    // todo : properly handle parameters and move this to send()
-
-    if (action == "message") {
-      var token = process.env["TROPO_MESSAGE_TOKEN"];
-    } else if (action == "call") {
-      var token = process.env["TROPO_CALL_TOKEN"];
-    }
-
-    var session = new TropoSession();
-    //session.makeApiCall(token, { msg: message, number: number });
-
-    session.addListener('responseBody', function(response) {
-      console.log(response);
-    });
-
-    res.render("messages/send", {
-      title: "New message"
-    });
+    res.render("messages/send", { title: "New message" });
   },
 
   // /messages/send
   send: function(req, res) {
     var that = this;
 
+    // saving message to mongodb
     this.Messages.save(req.body.message, function(message, validationErrors) {
       if (validationErrors) {
+        // if there's validation errors with the form, send back the form with the errors
         that.validator.addErrorsToMessages(req, validationErrors);
+        res.render("messages/send", { title: "New message", message: message });
       } else {
-        // todo: api call here
-        that.messages.addMessage(req, "success", "Message sent with success.");
-        res.redirect("/");
+        console.log(message);
+
+        // todo : fix sms (problems seems to be about outbound sms)
+        // todo : reselect action when form has errors
+
+        var session = new TropoSession();
+
+        // selecting which token to use
+        if (message.messageType == "message") {
+          var token = process.env["TROPO_MESSAGE_TOKEN"];
+        } else if (message.messageType == "call") {
+          var token = process.env["TROPO_CALL_TOKEN"];
+        }
+
+        // sending message to API
+        session.makeApiCall(token, { msg: message.message, number: message.phoneNumber });
+
+        // reponse from Tropo API, sending success message
+        session.addListener('responseBody', function(response) {
+          that.messages.addMessage(req, "success", "Message sent with success.");
+          res.redirect("/");
+        });
+
+        // if the Tropo API does not answer, we send a timeout message.
+        setTimeout(function () { 
+          that.messages.addMessage(req, "error", "Message has timed out. Please try again");
+          res.render("messages/send", { title: "New message", message: message });
+        }, 10000)
+
       }
-
-      res.render("messages/send", {
-        title: "New message",
-        message: message
-      });
-
     });
   },
 
