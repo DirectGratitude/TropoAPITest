cc90417edcf2c1bace668dc1955821b34c02ae3a
Tommy Bergeron <tomshreds@me.com>
2012-06-06 19:54:44 -0400

    removed useless comments

--- app/controllers/message.js
+++ app/controllers/message.js
@@ -25,12 +25,6 @@ var MessageController = createController({
         that.validator.addErrorsToMessages(req, validationErrors);
         res.render("messages/send", { title: "New message", message: message });
       } else {
-        console.log(message);
-
-        // todo: add lifestore vhost to nginx
-        // todo: finish setting up monit
-        // todo: checkout mmonit
-
         // todo : fix sms (problems seems to be about outbound sms)
         // todo : reselect action when form has errors
 
@@ -53,10 +47,10 @@ var MessageController = createController({
         });
 
         // if the Tropo API does not answer, we send a timeout message.
-//        setTimeout(function () { 
-//          that.messages.addMessage(req, "error", "Message has timed out. Please try again");
-//          res.render("messages/send", { title: "New message", message: message });
-//        }, 10000);
+        setTimeout(function () { 
+          that.messages.addMessage(req, "error", "Message has timed out. Please try again");
+          res.render("messages/send", { title: "New message", message: message });
+        }, 30000);
 
       }
     });
